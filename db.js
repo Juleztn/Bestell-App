3cc4fe627fccd5c14e989c627e51355b4be13466
Jule Zieten <ju-zie@web.de>
2025-07-10 19:19:54 +0200

    fonts, images, icons added, JSON object created

---
+++ db.js
@@ -0,0 +1,100 @@
+let meals = {
+    "categories": [ 
+        {
+            "name": "Pizza",
+            "image": "../assets/img/pizza.jpg",
+            "items": [
+                {
+                    "name": "Pizza Margherita",
+                    "description": "Mit Tomatensauce, Mozzarella und frischem Basilikum",
+                    "price": 8.50,
+                    "image": "../assets/img/pizza-margherita.jpg"
+                },
+                {
+                    "name": "Pizza Capricciosa",
+                    "description": "Mit Tomaten, Mozzarella, Basilikum, Champignons und Oliven",
+                    "price": 10.20,
+                    "image": "../assets/img/pizza-capricciosa.jpg"
+                },
+                {
+                    "name": "Pizza Salami",
+                    "description": "Mit Rindersalami",
+                    "price": 9.50,
+                    "image": "../assets/img/pizza-salami.jpg"
+                }
+            ]
+        },
+        {
+            "name": "Pasta",
+            "image": "../assets/img/pasta.jpg",
+            "items": [
+                {
+                    "name": "Pasta al Pomodoro",
+                    "description": "Spaghetti mit Tomatensauce",
+                    "price": 9.50,
+                    "image": "../assets/img/pasta-al-pomodoro.jpg"
+                },
+                {
+                    "name": "Fettuccine Garnelen",
+                    "description": "Mit Garnelen und Rucola",
+                    "price": 11.90,
+                    "image": "../assets/img/fettuccine-garnelen.jpg"
+                },
+                {
+                    "name": "Spaghetti Bolognese",
+                    "description": "Mit Tomatensauce und Rinderhackfleisch",
+                    "price": 10.50,
+                    "image": "../assets/img/spaghetti-bolognese.jpg"
+                }
+            ]
+        },
+        {
+            "name": "Beilagen",
+            "image": "../assets/img/beilagen.jpg",
+            "items": [
+                {
+                    "name": "Pommes",
+                    "price": 4.50,
+                    "image": "../assets/img/pommes.jpg"
+                },
+                {
+                    "name": "Tomate-Mozzarella Salat",
+                    "price": 4.20,
+                    "image": "../assets/img/tomate-mozzarella.jpg"
+                },
+                {
+                    "name": "Gemischte Salat",
+                    "description": "Mit Zwiebeln, Paprika und Oliven",
+                    "price": 5.50,
+                    "image": "../assets/img/gemischter-salat.jpg"
+                }
+            ]
+        },
+        {
+            "name": "Getränke",
+            "image": "../assets/img/getränke.jpg",
+            "items": [
+                {
+                    "name": "Cola",
+                    "description": "0,33 Liter",
+                    "price": 2.40
+                },
+                {
+                    "name": "Fanta",
+                    "description": "0,33 Liter",
+                    "price": 2.40,
+                },
+                {
+                    "name": "Sprite",
+                    "description": "0,33 Liter",
+                    "price": 2.40
+                },
+                {
+                    "name": "Wasser",
+                    "description": "1 Liter",
+                    "price": 3.00
+                }
+            ]
+        }
+    ]
+}
