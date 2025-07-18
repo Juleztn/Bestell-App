6374f0eadd4eb8f2a59ae2511126385ea8487ddc
Jule Zieten <ju-zie@web.de>
2025-07-18 13:25:46 +0200

    db.js changed images

--- scripts/db.js
+++ scripts/db.js
@@ -2,77 +2,77 @@ let meals = {
     "categories": [ 
         {
             "name": "Pizza",
-            "image": "../assets/img/pizza.jpg",
+            "image": "./assets/img/pizza.jpg",
             "items": [
                 {
                     "name": "Pizza Margherita",
                     "description": "Mit Tomatensauce, Mozzarella und frischem Basilikum",
                     "price": 8.50,
-                    "image": "../assets/img/pizza-margherita.jpg"
+                    "image": "./assets/img/pizza-margherita.jpg"
                 },
                 {
                     "name": "Pizza Capricciosa",
                     "description": "Mit Tomaten, Mozzarella, Basilikum, Champignons und Oliven",
                     "price": 10.20,
-                    "image": "../assets/img/pizza-capricciosa.jpg"
+                    "image": "./assets/img/pizza-capricciosa.jpg"
                 },
                 {
                     "name": "Pizza Salami",
                     "description": "Mit Rindersalami",
                     "price": 9.50,
-                    "image": "../assets/img/pizza-salami.jpg"
+                    "image": "./assets/img/pizza-salami.jpg"
                 }
             ]
         },
         {
             "name": "Pasta",
-            "image": "../assets/img/pasta.jpg",
+            "image": "./assets/img/pasta.jpg",
             "items": [
                 {
                     "name": "Pasta al Pomodoro",
                     "description": "Spaghetti mit Tomatensauce",
                     "price": 9.50,
-                    "image": "../assets/img/pasta-al-pomodoro.jpg"
+                    "image": "./assets/img/pasta-al-pomodoro.jpg"
                 },
                 {
                     "name": "Fettuccine Garnelen",
                     "description": "Mit Garnelen und Rucola",
                     "price": 11.90,
-                    "image": "../assets/img/fettuccine-garnelen.jpg"
+                    "image": "./assets/img/fettuccine-garnelen.jpg"
                 },
                 {
                     "name": "Spaghetti Bolognese",
                     "description": "Mit Tomatensauce und Rinderhackfleisch",
                     "price": 10.50,
-                    "image": "../assets/img/spaghetti-bolognese.jpg"
+                    "image": "./assets/img/spaghetti-bolognese.jpg"
                 }
             ]
         },
         {
             "name": "Beilagen",
-            "image": "../assets/img/beilagen.jpg",
+            "image": "./assets/img/beilagen.jpg",
             "items": [
                 {
                     "name": "Pommes",
                     "price": 4.50,
-                    "image": "../assets/img/pommes.jpg"
+                    "image": "./assets/img/pommes.jpg"
                 },
                 {
                     "name": "Tomate-Mozzarella Salat",
                     "price": 4.20,
-                    "image": "../assets/img/tomate-mozzarella.jpg"
+                    "image": "./assets/img/tomate-mozzarella.jpg"
                 },
                 {
                     "name": "Gemischte Salat",
                     "description": "Mit Zwiebeln, Paprika und Oliven",
                     "price": 5.50,
-                    "image": "../assets/img/gemischter-salat.jpg"
+                    "image": "./assets/img/gemischter-salat.jpg"
                 }
             ]
         },
         {
             "name": "Getränke",
-            "image": "../assets/img/getränke.jpg",
+            "image": "./assets/img/getränke.jpg",
             "items": [
                 {
                     "name": "Cola",
